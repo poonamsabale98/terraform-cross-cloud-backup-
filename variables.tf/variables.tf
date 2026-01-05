96d9410058b9e79155ea77f0439d3f616588f1e2
POONAM <poonamsabale413709@gmail.com>
2026-01-05 17:21:15 +0530

    345

--- variables.tf/variables.tf
+++ variables.tf/variables.tf
@@ -1,8 +1,41 @@
-variable "aws_region" { ap-southeast-2}
-variable "s3_bucket_name" { myaws_s3bucket }
-variable "azure_subscription_id" { type = string }
-variable "azure_tenant_id" { type = string }
-variable "azure_location" { Central India }
-variable "azure_rg_name" { type = string }
-variable "azure_sa_name" { type = string }
-variable "azure_container_name" { s3backup }
+# AWS settings
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the existing S3 bucket to back up"
+  type        = string
+}
+
+# Azure settings
+variable "azure_subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "azure_tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "azure_location" {
+  description = "Azure region where resources will be created"
+  type        = string
+}
+
+variable "azure_rg_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "azure_sa_name" {
+  description = "Name of the Azure Storage Account (must be unique, lowercase)"
+  type        = string
+}
+
+variable "azure_container_name" {
+  description = "Name of the Azure Blob container"
+  type        = string
+}
